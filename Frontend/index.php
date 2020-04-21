186e8ed490bbdfa8d10a968d7cbdd3ec28cbbbce
job26a <job.mekkelholt@student.graafschapcollege.nl>
2020-04-21 09:11:17 +0200

    bruh

--- Frontend/index.php
+++ Frontend/index.php
@@ -14,17 +14,17 @@
     <div id="header">
       <div id="logo">
         <!-- class="logo_colour", allows you to change the colour of the text -->
-        <h1><a href="index.html">simple<span class="logo_colour">_light</span></a></h1>
+        <h1><a href="index.php">simple<span class="logo_colour">_light</span></a></h1>
         <h2>Simple. Contemporary. Website Template.</h2>
       </div>
       <div id="menubar">
         <ul id="menu">
           <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
-          <li class="selected"><a href="index.html">Home</a></li>
-          <li><a href="examples.html">Examples</a></li>
-          <li><a href="page.html">A Page</a></li>
-          <li><a href="another_page.html">Another Page</a></li>
-          <li><a href="contact.html">Contact Us</a></li>
+          <li class="selected"><a href="index.php">Home</a></li>
+          <li><a href="examples.php">Examples</a></li>
+          <li><a href="page.php">A Page</a></li>
+          <li><a href="another_page.php">Another Page</a></li>
+          <li><a href="contact.php">Contact Us</a></li>
         </ul>
       </div>
     </div>
@@ -54,7 +54,7 @@
         <p>This standards compliant, simple, fixed width website template is released as an 'open source' design (under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...</p>
         <p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS</strong>, and can be validated using the links in the footer.</p>
         <p>You can view more free HTML5 web templates <a href="http://www.html5webtemplates.co.uk">here</a>.</p>
-        <p>This template is a fully functional 5 page website, with an <a href="examples.html">examples</a> page that gives examples of all the styles available with this design.</p>
+        <p>This template is a fully functional 5 page website, with an <a href="examples.php">examples</a> page that gives examples of all the styles available with this design.</p>
         <h2>Browser Compatibility</h2>
         <p>This template has been tested in the following browsers:</p>
         <ul>
@@ -65,7 +65,7 @@
       </div>
     </div>
     <div id="footer">
-      <p><a href="index.html">Home</a> | <a href="examples.html">Examples</a> | <a href="page.html">A Page</a> | <a href="another_page.html">Another Page</a> | <a href="contact.html">Contact Us</a></p>
+      <p><a href="index.php">Home</a> | <a href="examples.php">Examples</a> | <a href="page.php">A Page</a> | <a href="another_page.php">Another Page</a> | <a href="contact.php">Contact Us</a></p>
       <p>Copyright &copy; simple_light | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">design from HTML5webtemplates.co.uk</a></p>
     </div>
   </div>
