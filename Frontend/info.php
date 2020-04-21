775fce36d32905af5a8a2c36a326b3a1f2bfcc17
Gijser <57718086+Gijser@users.noreply.github.com>
2020-04-21 11:38:21 +0200

    infoPagina bezig

--- Frontend/info.php
+++ Frontend/info.php
@@ -2,73 +2,150 @@
 <html>
 
 <head>
-  <title>simple_light</title>
-  <meta name="description" content="website description" />
-  <meta name="keywords" content="website keywords, website keywords" />
-  <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-  <link rel="stylesheet" type="text/css" href="style/style.css" />
+    <title>simple_light</title>
+    <meta name="description" content="website description" />
+    <meta name="keywords" content="website keywords, website keywords" />
+    <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
 </head>
 
 <body>
-  <div id="main">
-    <div id="header">
-      <div id="logo">
-        <!-- class="logo_colour", allows you to change the colour of the text -->
-        <h1><a href="index.php">simple<span class="logo_colour">_light</span></a></h1>
-        <h2>Simple. Contemporary. Website Template.</h2>
-      </div>
-      <div id="menubar">
-        <ul id="menu">
-          <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
-          <li><a href="index.php">Home</a></li>
-          <li><a href="bezigheden.php">Bezigheden</a></li>
-          <li><a href="besmetting.php">Besmetting</a></li>
-          <li><a href="webshop.php">Webshop</a></li>
-          <li><a href="risicogroep.php">Risicogroep</a></li>
-          <li class="selected"><a href="info.php">informatie</a></li>
-        </ul>
-      </div>
+    <div id="main">
+        <div id="header">
+            <div id="logo">
+                <!-- class="logo_colour", allows you to change the colour of the text -->
+                <h1><a href="index.php">simple<span class="logo_colour">_light</span></a></h1>
+                <h2>Simple. Contemporary. Website Template.</h2>
+            </div>
+            <div id="menubar">
+                <ul id="menu">
+                    <!-- put class="selected" in the li tag for the selected page - to highlight which page you're on -->
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="bezigheden.php">Bezigheden</a></li>
+                    <li><a href="besmetting.php">Besmetting</a></li>
+                    <li><a href="webshop.php">Webshop</a></li>
+                    <li><a href="risicogroep.php">Risicogroep</a></li>
+                    <li class="selected"><a href="info.php">informatie</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="site_content">
+            <div class="sidebar">
+
+                <h1>Latest News</h1>
+                <h4>New Website Launched</h4>
+                <h5>January 1st, 2010</h5>
+                <p>2010 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
+                <h1>Useful Links</h1>
+                <ul>
+                    <li><a href="#">link 1</a></li>
+                    <li><a href="#">link 2</a></li>
+                    <li><a href="#">link 3</a></li>
+                    <li><a href="#">link 4</a></li>
+                </ul>
+                <h1>Search</h1>
+                <form method="post" action="#" id="search_form">
+                    <p>
+                        <input class="search" type="text" name="search_field" value="Enter keywords....." />
+                        <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
+                    </p>
+                </form>
+            </div>
+            <div id="content">
+                <h1>Hoe kun je Corona voorkomen?</h1>
+                <p>
+                    <ul>
+                        <li>Blijf zoveel mogelijk thuis.
+                            <ul>
+                                <li>
+                                    ontvang zo min mogelijk bezoek(Maximaal 3 bezoekers) en
+                                </li>
+                                <li>
+                                    Houd dan 1,5 meter afstand tot elkaar.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            Ga alleen naar buiten als dat nodig is.
+                            <ul>
+                                <li>
+                                    Voor werk wanneer je niet thuis kan werken, voor boodschappen, een frisse neus of om iets voor een ander te doen.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            Ga niet op bezoek bij mensen van 70 jaar of ouder en mensen met een kwetsbare gezondheid.
+                            <ul>
+                                <li>
+                                    Ga zeker niet op bezoek als je verkoudhijdsklachten hebt.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            Verpleeghuizen zijn gesloten voor bezoek.
+                        </li>
+                        <li>
+                            als je naar buiten gaat, houd dan tenminste 1,5 meter adstand tot anderen(uitgezonderd gezinnen/huishoudens en kinderen tot en met 12 jaar).
+                        </li>
+                        <li>
+                            Tegen groepen van meer dan 2 personen die niet 1,5 meter afstand van elkaar houden, kan worden opgetreden.
+                        </li>
+                        <li>
+                            Zorg voor goede hygiënemaatregelen:
+                            <ul>
+                                <li>
+                                    Was je handen.
+                                    <ul>
+                                        <li>
+                                            20 seconden lang met water en zeep, daarna je handen goed dragen.
+                                        </li>
+                                        <li>
+                                            Voordat je naar buiten gaat, als je weer thuis komt als je je neus hebt gesnoten, natuurlijk voor het eten en nadat je naar de wc bent geweest.
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    Hoest en nies in de binnenkant van je elleboog.
+                                </li>
+                                <li>
+                                    Gebruik papieren zakdoekjes om je neus te snuiten en gooi deze daarna weg
+                                </li>
+                                <li>
+                                    Was daarna je handen
+                                </li>
+                                <li>
+                                    Schud geen handen
+                                </li>
+                                <li>
+                                    houd 1,5 meter afstand(2 armlengtes) van anderen
+                                    <ul>
+                                        <li>
+                                            Dit geldt voor iedereen, bijveerbeeld op straat, in winkels, met collega's, behalve thuis en binnen het gezin/huishouden.
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+
+                </p>
+                <p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS</strong>, and can be validated using the links in the footer.</p>
+                <p>You can view more free HTML5 web templates <a href="http://www.html5webtemplates.co.uk">here</a>.</p>
+                <p>This template is a fully functional 5 page website, with an <a href="examples.php">examples</a> page that gives examples of all the styles available with this design.</p>
+                <h2>Browser Compatibility</h2>
+                <p>This template has been tested in the following browsers:</p>
+                <ul>
+                    <li>Internet Explorer 8</li>
+                    <li>FireFox 3</li>
+                    <li>Google Chrome 13</li>
+                </ul>
+            </div>
+        </div>
+        <div id="footer">
+            <p><a href="index.php">Home</a> | <a href="webshop.php">Webshop</a> | <a href="informatie.php">Info</a> | <a href="besmetting.php">Besmetting</a> | <a href="bezigheden.php">Bezigheden</a></p>
+            <p>Copyright &copy; simple_light | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">design from HTML5webtemplates.co.uk</a></p>
+        </div>
     </div>
-    <div id="site_content">
-      <div class="sidebar">
-        <h1>Latest News</h1>
-        <h4>New Website Launched</h4>
-        <h5>January 1st, 2010</h5>
-        <p>2010 sees the redesign of our website. Take a look around and let us know what you think.<br /><a href="#">Read more</a></p>
-        <h1>Useful Links</h1>
-        <ul>
-          <li><a href="#">link 1</a></li>
-          <li><a href="#">link 2</a></li>
-          <li><a href="#">link 3</a></li>
-          <li><a href="#">link 4</a></li>
-        </ul>
-        <h1>Search</h1>
-        <form method="post" action="#" id="search_form">
-          <p>
-            <input class="search" type="text" name="search_field" value="Enter keywords....." />
-            <input name="search" type="image" style="border: 0; margin: 0 0 -9px 5px;" src="style/search.png" alt="Search" title="Search" />
-          </p>
-        </form>
-      </div>
-      <div id="content">
-        <h1>Welcome to the simple_light template</h1>
-        <p>This standards compliant, simple, fixed width website template is released as an 'open source' design (under a <a href="http://creativecommons.org/licenses/by/3.0">Creative Commons Attribution 3.0 Licence</a>), which means that you are free to download and use it for anything you want (including modifying and amending it). All I ask is that you leave the 'design from HTML5webtemplates.co.uk' link in the footer of the template, but other than that...</p>
-        <p>This template is written entirely in <strong>HTML5</strong> and <strong>CSS</strong>, and can be validated using the links in the footer.</p>
-        <p>You can view more free HTML5 web templates <a href="http://www.html5webtemplates.co.uk">here</a>.</p>
-        <p>This template is a fully functional 5 page website, with an <a href="examples.php">examples</a> page that gives examples of all the styles available with this design.</p>
-        <h2>Browser Compatibility</h2>
-        <p>This template has been tested in the following browsers:</p>
-        <ul>
-          <li>Internet Explorer 8</li>
-          <li>FireFox 3</li>
-          <li>Google Chrome 13</li>
-        </ul>
-      </div>
-    </div>
-    <div id="footer">
-      <p><a href="index.php">Home</a> | <a href="webshop.php">Webshop</a> | <a href="informatie.php">Info</a> | <a href="besmetting.php">Besmetting</a> | <a href="bezigheden.php">Bezigheden</a></p>
-      <p>Copyright &copy; simple_light | <a href="http://validator.w3.org/check?uri=referer">HTML5</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.html5webtemplates.co.uk">design from HTML5webtemplates.co.uk</a></p>
-    </div>
-  </div>
 </body>
+
 </html>
