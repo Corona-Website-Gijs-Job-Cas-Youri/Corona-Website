5016161e92c93fdcadc758fe8c60bef211ce803e
Gijser <57718086+Gijser@users.noreply.github.com>
2020-04-23 08:43:59 +0200

    informatie nog niet af

--- Frontend/info.php
+++ Frontend/info.php
@@ -46,7 +46,7 @@
                 </ul>
 
             </div>
-            <div id="content">
+            <div id="info_content">
                 <h1>Hoe kun je Corona voorkomen?</h1>
                 <p>
                     <ul>
@@ -124,10 +124,13 @@
                     </li>
                     </ul>
                 </p>
+                <div id="info_content">
                 <h1>Ik heb klachten die passen bij het nieuwe coronavirus. Wat moet ik doen?</h1>
-                <p>
-                    help
-                </p>
+                <li>
+                    <b>Heb je verkoudheidsklachten, zoals neusverkoudheid, loopneus, keelpijn, lichte hoest of verhoging (tot 38 graden Celsius)? </b>
+                    Blijf thuis. Ziek uit. Doe geen boodschappen en ontvang geen bezoek. Laat anderen boodschappen doen, of laat ze bezorgen, zorg dat een ander de hond uitlaat. Voor huisgenoten zonder klachten gelden de regels die voor iedereen in Nederland gelden. Als je 24 uur lang geen klachten hebt, mag je weer naar buiten. 
+                </li>
+                </div>
             </div>
         </div>
         <div id="footer">
