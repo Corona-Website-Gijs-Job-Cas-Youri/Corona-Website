cf88b18840599a299436a7c0125cb9467dfa2df9
Gijser <57718086+Gijser@users.noreply.github.com>
2020-05-07 10:53:02 +0200

    mondkapje

--- Frontend/winkelwagen.php
+++ Frontend/winkelwagen.php
@@ -7,7 +7,7 @@
 <html>
 
 <head>
-  <title>mondkapjes</title>
+  <title>Winkelwagen</title>
   <meta name="description" content="website description" />
   <meta name="keywords" content="website keywords, website keywords" />
   <meta http-equiv="content-type" content="text/html; charset=windows-1252" />
