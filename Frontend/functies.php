1d4c73ee78b55064e359abde2aae83dcf8dc1fab
Gijser <57718086+Gijser@users.noreply.github.com>
2020-05-07 10:36:50 +0200

    locatie func begin v cas

--- Frontend/functies.php
+++ Frontend/functies.php
@@ -1,1 +1,12 @@
 <?php require_once('db_connect.php');
+
+function locatie()
+{
+    
+    $dbhost = "localhost";
+    $dbuser = "gheetebrij_gheetebrij";
+    $dbpass = "bHdmHP1FrKHH";
+    $db = "gheetebrij_Corona";
+
+
+}
