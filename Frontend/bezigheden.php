4857258fbdd2fdf6b3e8e0998f896d1c51c90e0a
D3ADFAC3D <y.blanckenborg@gmail.com>
2020-05-07 10:53:15 +0200

    Bezigheden bijgewerkt

--- Frontend/bezigheden.php
+++ Frontend/bezigheden.php
@@ -48,64 +48,75 @@
       <div id="content">
           <p>
           <h1>Wat te doen tijdens quarantaine</h1>
-          Verveelt uw zich tijdens quarantaine veroorzaakt door de momemtele uitbreek van covid 19.<br>
-          Dan zijn hier uw mogelijke dingen om uw bezigheden:<br>
-          <br>
-          <h1>Sport</h1><br>
-          Het is in deze tijden belangrijk dat je gezond blijft, dus hier zijn een paar sportoefeningen voor thuis.<br>
+          Heeft u ook zo'n last van verveling tijdens deze quarantaine? <br>
+          Dan hebben wij mogelijk de oplossing! Hier een paar ideeën tegen de verveling:
+          <h1>Sport</h1>
+          <p>Het is deze tijden erg belangrijk dat je gezond blijft. Daarnaast houdt je door het sporten je geest en je lichaam fit en is het binnenzitten beter te verdragen.
+          Daarom hebben wij een paar oefeningen op een rijtje gezet:
+          </p>
           
           <ul>
-          <li><h4>Touwtje springen</h4><br>
-          Het is een oefening om je conditie op niveau te houden. dit kan je makkelijkste doen met echt springtouw dat je waarschijnlijk
-          hebt als je kinderen hebt of minder makkelijk met een normaal touw als alternatief voor een springtouw gebruiken.<br>
-          klik <a href="https://www.afvallenmetsport.nl/touwtje-springen-6-tips-om-het-te-leren/">hier</a> voor tips 
-          bij het touwtje sprigen</li>          
-          <li><h4>Opdrukken</h4><br>
-          Het is een goede oefening om je armspieren sterk te houden.
-          Klik <a href="https://voeding-en-fitness.nl/push-ups-uitvoering-techniek-tips/">hier</a> voor tips bij het opdrukken.</li>
-          <li><h4>Planken</h4><br>
-          Een goed alternatief voor opdrukken.
-          Klik <a href="https://drogespieren.nl/krachttraining/planking">hier</a> voor tips bij het Planken</li>
-          <li><h4>Hardlopen</h4><br>
-          Het is een goede afleiding van de crisis waarbij je je conditie opbouwt en wat natuur ziet tijdens de oefening.<br>
-          Klik <a href="https://sochicken.nl/beginnen-met-hardlopen">hier</a> voor tips bij het hardlopen</li>
-          <li><h4>Yoga</h4><br>
-          Het helpt je ontspannen en lenig te worden.
-          Klik hier <a href="https://www.yogashop.nl/nl/blogs/yoga-en-meditatie/yoga-voor-beginners/">hier</a> voor tips bij yoga.</li>
-          <li><h4>squats</h4><br>
-          Een goede oefeningen voor de beenspieren.<br>
-          Klik <a href="https://www.fit.nl/fitnessoefeningen/squat">hier</a> voor tips bij sqauts.</li>
-          
-          </ul><br>
-          
-          <h1>Relaxen</h1><br>
-          Je moet natuurlijk ook relaxen. hier zijn een paar dingen die kunt doen:<br>
-          <br>
-          <ul>
-          <li><h4>Kijk een film, klik dan <a href="https://www.filmvandaag.nl/top100">hier</a> voor voorbeelden</h4><br>
-          een film is een goede afleiding die je ook met het hele gezin kunt kijken.</li>
-          <li><h4>Klus aan het huis</h4><br>
-          Nu je toch meer tijd kun je die tijd gebruiken om aan je huis te werken als dat mag.</li>
-          <li><h4>Zit buiten op je terras(als je dat hebt)</h4><br>
-          je kunt natuurlijk ook gewoon buiten zitten met wat te drinken en van de zon genieten</li>
-          <li><h4>Tuinieren</h4><br>
-          Met de coronacrisis kun je eindelijk wat tijd besteden aan die tuin of moestuin in je achtertuin</li>
-          <li><h4>Doe zelf iets anders wat je leuk</h4><br>
-          Dit zijn maar een paar voorstellen die benoemd zijn, maar uw kunt natuurlijk ook zelf iets bedenken</li>
-          </ul><br>
+          <li><h4>Touwtje springen</h4>
+          <p>Het is een oefening om je conditie op niveau te houden. Het beste is om goed springtouw te gebruiken, anders werkt een normaal stuk touw ook voldoende.</p>
+          Klik <a href="https://www.afvallenmetsport.nl/touwtje-springen-6-tips-om-het-te-leren/" target="_blank">hier</a> voor tips 
+          bij het touwtje springen</li> 
 
-          <h1>Leer iets nieuws</h1><br>
-          Je kunt deze tijd die iedereen krijgt ook gebruiken om nieuwe dingen te leren:<br>
+          <li><h4>Opdrukken</h4>
+          <p>Het is een goede oefening om je armspieren en borstspieren sterk te houden.</p>
+          Klik <a href="https://voeding-en-fitness.nl/push-ups-uitvoering-techniek-tips/">hier</a> voor tips bij het opdrukken.</li>
+
+          <li><h4>Planken</h4>
+          <p>Een simpele en uitstekende oefening voor jong en oud. Deze oefening traint de buikspieren.</p>
+          Klik <a href="https://drogespieren.nl/krachttraining/planking">hier</a> voor tips bij het Planken</li>
+
+          <li><h4>Hardlopen</h4>
+          <p>Het is een goede afleiding van de crisis waarbij je je conditie opbouwt en even lekker een frisse neus kan halen.</p>
+          Klik <a href="https://sochicken.nl/beginnen-met-hardlopen">hier</a> voor tips bij het hardlopen</li>
+
+          <li><h4>Yoga</h4>
+          <p>Het helpt je ontspannen en lenig te worden.</p>
+          Klik hier <a href="https://www.yogashop.nl/nl/blogs/yoga-en-meditatie/yoga-voor-beginners/">hier</a> voor tips bij yoga.</li>
+
+          <li><h4>Squats</h4>
+          <p>Een goede oefening voor de beenspieren.</p>
+          Klik <a href="https://www.fit.nl/fitnessoefeningen/squat">hier</a> voor tips bij het squatten.</li>
+          
+          </ul>
+          
+          <h1>Relaxen</h1>
+          <p>Je moet natuurlijk ook relaxen. Hier zijn een paar dingen die kunt doen:</p>
           <ul>
-            <li><h4>Leer een instrument spelen</h4><br>
-            Hebt uw misschien een nieuwe interesse in muziek, dan kunt uw nu focussen op het beheersen van een instrument en 
-            mogelijk tegelijkertijd leren zingen.</li>
-            <li><h4>Ontdek jezelf</h4><br>
-            Misschien ontdek je jezelf door het lange thuis zitten of door het verkennen van nieuwe dingen, 
-            zoals bijvoorbeeld een nieuwe passie of nieuwe interesse</li>
-            <li><h4>Leer iets over planten en dieren</h4><br>
-            Met deze hoeveelheid tijd kunt uw tijd investeren in de dieren en planten wereld en bijvoorbeeld iets nieuws over uw 
-            kat of hond leren of een nieuwe eigenschap of feit van uw huisplant.</li>
+          <li><h4>Kijk een film</h4>
+          <p>Een film is een goede afleiding die je ook met het hele gezin kunt kijken.</p>
+          <p>klik <a href="https://www.filmvandaag.nl/top100">hier</a> voor voorbeelden</p></li>
+
+          <li><h4>Klussen</h4>
+          <p>Nu je toch meer tijd hebt, kun je die tijd mooi gebruiken om aan je huis of tuin te werken.</p></li>
+
+          <li><h4>Ga buiten zitten</h4>
+          <p>Je kunt natuurlijk ook gewoon buiten zitten met wat te drinken en van de zon genieten</p></li>
+
+          <li><h4>Tuinieren</h4>
+          <p>Met de coronacrisis kun je eindelijk wat tijd besteden aan die tuin of moestuin in je achtertuin</p></li>
+
+          <li><h4>Ga op zoek</h4>
+          <p>Ga kijken wat je leuk vindt, misschien ontdek je wel een nieuwe hobby.</p></li>
+          </ul>
+
+          <h1>Leer iets nieuws</h1>
+          <p> kunt deze tijd ook gebruiken om nieuwe dingen te leren:</p>
+          <ul>
+            <li><h4>Leer een instrument spelen</h4>
+            <p>Hebt uw misschien een nieuwe interesse in muziek, dan kunt uw zich nu focussen op het beheersen van een instrument en 
+            mogelijk tegelijkertijd leren zingen.</p></li>
+
+            <li><h4>Ontdek jezelf</h4>
+            <p>Misschien ontdek je jezelf door het lange thuis zitten of door het verkennen van nieuwe dingen, 
+            zoals bijvoorbeeld een nieuwe passie of nieuwe interesse</p></li>
+
+            <li><h4>Leer iets over planten en dieren</h4>
+            <p>Met deze hoeveelheid tijd kunt uw tijd investeren in de dieren en planten wereld en bijvoorbeeld iets nieuws over uw 
+            kat of hond leren of een nieuwe eigenschap of feit van uw huisplant.</p></li>
           </ul>
           </p>        
       </div>
