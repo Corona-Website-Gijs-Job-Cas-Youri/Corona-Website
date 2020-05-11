b97928ec19d3428c8ec197f52dd5450a6582bab6
D3ADFAC3D <y.blanckenborg@gmail.com>
2020-05-11 12:48:33 +0200

    Webshop beschrijving aangepast

--- Frontend/handschoenen.php
+++ Frontend/handschoenen.php
@@ -58,7 +58,8 @@
       <form method="post">
         <input type="number" min="1" max="10" name="aantal"><button name="toevoegen">aan winkel wagen toevoegen</button>
       
-      <div id="beschrijving"><p>Sterke nitril wegwerphandschoenen van een soepel materiaal voor een optimale bescherming met veel draagcomfort, een goede pasvorm en een goed tastgevoel.</p></div>
+      <div id="beschrijving"><p>Sterke nitril wegwerphandschoenen van een soepel materiaal. <br>
+        Voor een optimale bescherming met veel draagcomfort, een goede pasvorm en een goed tastgevoel.</p></div>
 
           </div>
           <?php
