3926006c9a98ae3de9be0ed97d8d67b9a8e7399c
Gijser <57718086+Gijser@users.noreply.github.com>
2020-05-12 09:23:44 +0200

    echo voor laten zien

--- Frontend/functies.php
+++ Frontend/functies.php
@@ -8,4 +8,5 @@ function locatie()
     $sql = "SELECT `Aantal_Besmet` FROM `besmet` WHERE `Provincie` == $provincie";
     $result = mysqli_query(OpenCon(), $sql);
     $output = mysqli_fetch_assoc($result);
+    echo "In " + $provincie + " zijn " + $output + " mensen besmet";
 }
