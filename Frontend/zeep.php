1ba0aca3c6344c3b22242d9340a748194ae50975
Gijser <57718086+Gijser@users.noreply.github.com>
2020-05-14 14:11:01 +0200

    no

--- Frontend/zeep.php
+++ Frontend/zeep.php
@@ -69,8 +69,8 @@
           <?php
           if(isset($_POST['toevoegen'])){
             $_SESSION['product3'] = "zeep";
-            $_SESSION['bestelling3'] = $_POST['aantal3'];
-            $_SESSION['prijsTotaal3'] = 5 * $_POST['aantal3'];
+            $_SESSION['bestelling3'] = $_POST['aantal'];
+            $_SESSION['prijsTotaal3'] = 5 * $_POST['aantal'];
             $_SESSION['bestelling'] = 1;
 
             header("location: winkelwagen.php");
