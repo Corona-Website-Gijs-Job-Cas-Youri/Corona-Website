6fa9d18e45576b8d33a739fb90788596d52a4c8c
job26a <job.mekkelholt@student.graafschapcollege.nl>
2020-05-14 13:11:07 +0200

    bruh

--- Frontend/bestellen.php
+++ Frontend/bestellen.php
@@ -3,7 +3,6 @@
 
   require_once('db_conn.php');
 
-  OpenCon();
 
   if(isset($_POST['bestellen'])){
 
@@ -11,19 +10,19 @@
     $sql_insert1 = "INSERT INTO producten(Prod_Naam, Prod_Prijs)
     VALUES('{$_SESSION['product1']}', {$_SESSION['prijsTotaal1']})";
 
-    $result1 = mysqli_query($conn, $sql_insert1);
+    $result1 = mysqli_query(OpenCon(), $sql_insert1);
   }
   if(isset($_SESSION['bestelling2'])){
   $sql_insert2 = "INSERT INTO producten(Prod_Naam, Prod_Prijs)
     VALUES('{$_SESSION['product2']}', {$_SESSION['prijsTotaal2']})";
 
-    $result2 = mysqli_query($conn, $sql_insert2);
+    $result2 = mysqli_query(OpenCon(), $sql_insert2);
   }
   if(isset($_SESSION['bestelling3'])){
   $sql_insert3 = "INSERT INTO producten(Prod_Naam, Prod_Prijs)
     VALUES('{$_SESSION['product3']}', {$_SESSION['prijsTotaal3']})";
 
-    $result3 = mysqli_query($conn, $sql_insert3);
+    $result3 = mysqli_query(OpenCon(), $sql_insert3);
 }
 
 
@@ -33,7 +32,7 @@
     $sql_insert = "INSERT INTO bestelling(Email, Handschoenen, Mondkapjes, Zeep, Tot_Prijs) 
                     VALUES('{$_SESSION['Email']}', {$_SESSION['bestelling1']}, {$_SESSION['bestelling2']}, {$_SESSION['bestelling3']}, {$_SESSION['prijsTotaal']})";
 
-                    $result = mysqli_query($conn, $sql_insert);
+                    $result = mysqli_query((OpenCon()), $sql_insert);
                     
 
                     header("location: bevestig.php");
