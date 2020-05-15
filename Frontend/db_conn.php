c3e142d6a2071935d11399d8e323eba80a7d6cfd
Gijser <57718086+Gijser@users.noreply.github.com>
2020-05-15 08:50:39 +0200

    conn

--- Frontend/db_conn.php
+++ Frontend/db_conn.php
@@ -3,9 +3,9 @@
 function OpenCon()
 {
 $dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$db = "website";
+$dbuser = "gheetebrij_gheetebrij";
+$dbpass = "mxRKX8!qBZn2gUb^";
+$db = "gheetebrij_Corona";
 $conn = mysqli_connect($dbhost, $dbuser,$dbpass, $db) or die("Connect failed: %s\n");
 
 return $conn;
